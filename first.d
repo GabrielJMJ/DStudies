02c0a789c57c45d0001badd1a2380daa7d173379
Gabriel Jacinto <gamjj74@hotmail.com>
2015-02-12 21:30:49 -0200

    Add file

--- first.d
+++ first.d
@@ -1,15 +1,18 @@
 import std.stdio;
-
-class App
-{
-	public void run()
-	{
-		writefln("Running application...");
-	}
-}
+import std.net.curl;
+import std.json;
+import std.conv;
 
 void main()
 {
-	auto app = new App;
-	app.run();
+    char[] user;
+
+    write("What's the username on Facebook? ");
+    readln(user);
+
+    char[] requestContent = get("http://graph.facebook.com/" ~ user);
+    auto ctt = to!string(requestContent);
+    JSONValue[string] doc = parseJSON(ctt).object;
+
+    writeln("Name: ", doc["name"]);
 }
