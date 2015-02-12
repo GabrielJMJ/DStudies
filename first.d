7747e269873308807ede38418d10b8d7871558b6
Gabriel Jacinto <gamjj74@hotmail.com>
2015-02-12 21:31:54 -0200

    Flip file content

--- first.d
+++ first.d
@@ -1,18 +1,15 @@
 import std.stdio;
-import std.net.curl;
-import std.json;
-import std.conv;
+
+class App
+{
+    public void run()
+    {
+        writefln("Running application...");
+    }
+}
 
 void main()
 {
-    char[] user;
-
-    write("What's the username on Facebook? ");
-    readln(user);
-
-    char[] requestContent = get("http://graph.facebook.com/" ~ user);
-    auto ctt = to!string(requestContent);
-    JSONValue[string] doc = parseJSON(ctt).object;
-
-    writeln("Name: ", doc["name"]);
+    auto app = new App;
+    app.run();
 }
